4df2038708f5bb5a3850a510546ea583de116089
ninonesix <50695635+ninonesix@users.noreply.github.com>
2020-11-13 19:30:43 +0700

    Add files via upload

---
+++ controllers/indexController.js
@@ -0,0 +1,8 @@
+// const bookModel = require('../models/bookModel');
+
+exports.index = (req, res, next) => {
+    // Get from model
+    
+    // Pass data to view to display
+    res.render('index');
+};
