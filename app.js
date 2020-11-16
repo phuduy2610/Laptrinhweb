c82a8d4452ff450f86bb3fcce3b739ec4c1e0704
DESKTOP-M2N0JDQ\PC <mninonesix@gmail.com>
2020-11-16 16:32:22 +0700

    - update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs = require('hbs');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const registerRouter = require('./routes/register');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/register',registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
