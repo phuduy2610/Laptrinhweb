72355aad6def76175eff3cefb06fcefc16ddb7a2
ninonesix <mninonesix@gmail.com>
2020-11-17 10:30:35 +0700

    + update index controller and view, add index model

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,10 @@
-// const bookModel = require('../models/bookModel');
+const indexModel = require('../models/indexModel');
 
 exports.index = (req, res, next) => {
     // Get from model
-    
+    const slider = indexModel.slider();
+    const flexisel = indexModel.flexisel();
+    const genres = indexModel.genres();
     // Pass data to view to display
-    res.render('index');
+    res.render('index',{slider,flexisel,genres});
 };
